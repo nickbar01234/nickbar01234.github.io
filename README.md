8b2c72728eb8276a1b4852995b95f30b998880d3
nickbar01234 <nickbar01234@gmail.com>
2021-08-31 01:55:14 -0400

    The color theme hex codes are #141414, #FFF, #CCCCCC, #CFBB93, #8D8B87.
    I implement navbar, About section, and experience section. In addition,
    I add animation for the subtitle.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Personal Website 
+
+## Usage 
+
+`https://nickbar01234.github.io/` 
+
+## Acknowledgement 
+
+1) I chose my colour template using <a href="https://usefulangle.com/post/75/typing-effect-animation-javascript-css">Adobe Color wheel</a>. 
+
+2) For the subtitle animation, I referenced this <a href="https://usefulangle.com/post/75/typing-effect-animation-javascript-css">website</a>. 
+
+3) 
---
+++ js/index.js
@@ -0,0 +1,43 @@
+const position = [
+    "Machine Learning",
+    "Developer"
+]
+
+let step = 0;
+let substep = 0; 
+
+function increment() {
+
+    let text = position[step].substr(0, substep);
+    $("#subtitle p").html(text); 
+    
+    substep += 1;
+    if (text === position[step]) {
+        clearInterval(subtitle_animation);
+        setTimeout(function() {
+            subtitle_animation = setInterval(decrement, 50); 
+        }, 2000);
+    }
+}
+
+function decrement() {
+
+    let text = position[step].substr(0, substep - 1);
+    $("#subtitle p").html(text);
+
+    substep -= 1;
+    if (text === "") {
+
+        clearInterval(subtitle_animation);
+        
+        step += 1; 
+        step %= 2;
+        substep = 0; 
+
+        setTimeout(function() {
+            subtitle_animation = setInterval(increment, 100);
+        }, 200);    
+    }
+}
+
+let subtitle_animation = setInterval(increment, 100); 
